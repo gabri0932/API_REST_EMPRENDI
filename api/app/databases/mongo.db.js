661369e561a09064f7e923bffad44f00b6ab56d7
Darlin Arias <darlin.code@gmail.com>
2025-02-16 17:29:03 -0400

    conexion con la base de datos - correción

--- api/app/databases/mongo.db.js
+++ api/app/databases/mongo.db.js
@@ -1,15 +1,15 @@
 import { MongoClient } from "mongodb";
-import 'dotenv/config' //para usar las variables de entorno del .env
-import { error } from "console";
-const url = process.env.MONGO_URL //accedo a la variable de entorno del archivo .env
-console.log(url)
-const client = new MongoClient(url) //creo un nuevo cliente
-async function run(){
-    try{
-        const databases = process.env.DATABASE
-        return databases
-    }catch(error){
-        console.log(error)
+import 'dotenv/config' // para usar las variables de entorno del .env
+
+const url = process.env.MONGO_URL // accedo a la variable de entorno del archivo .env
+const client = new MongoClient(url) // creo un nuevo cliente
+
+export async function getMongoDB(){
+    try {
+        const database = client.db(process.env.DATABASE);
+        return database;
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
 }
-export default run
