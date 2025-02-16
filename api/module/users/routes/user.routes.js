16c315dc7a49203c704aec35c5785fddcdfc6b4f
Darlin Arias <darlin.code@gmail.com>
2025-02-16 19:06:19 -0400

    user controller y user routes - correción

--- api/module/users/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-import { UserController } from '../controllers/user.controllers' 
-const userRouter = Router()
-
-userRouter.get('/users/:userId', UserController.getUser)
-userRouter.patch('/users/:userId', UserController.updateUser)
-userRouter.delete('/users/:userId', UserController.deleteUser)
-
-export default userRouter;
