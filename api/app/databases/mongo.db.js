c2b7cc623ebc4c24df96bd057dec692b65c653e9
Darlin Arias <darlin.code@gmail.com>
2025-02-16 17:31:47 -0400

    conexion con la base de datos - correción 2

--- api/app/databases/mongo.db.js
+++ api/app/databases/mongo.db.js
@@ -2,10 +2,10 @@ import { MongoClient } from "mongodb";
 import 'dotenv/config' // para usar las variables de entorno del .env
 
 const url = process.env.MONGO_URL // accedo a la variable de entorno del archivo .env
-const client = new MongoClient(url) // creo un nuevo cliente
 
 export async function getMongoDB(){
     try {
+        const client = new MongoClient(url) // creo un nuevo cliente
         const database = client.db(process.env.DATABASE);
         return database;
     } catch (error) {
