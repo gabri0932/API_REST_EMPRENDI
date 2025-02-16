3565162eeacb2393560426e4ba559b49a362f640
gabri0932 <gabrielgarces2432@gmail.com>
2025-02-17 00:01:02 +0100

    user controller y user routes

---
+++ api/module/users/controllers/user.controllers.js
@@ -0,0 +1,6 @@
+export class UserController{
+    static getUser(){} //metodos son las cosas que puedes hacer en ese plano
+    static updateUser(){}
+    static deleteUser(){}
+} //las clases son como planos en los cuales puedes hacer diferentes cosas
+
--- api/module/users/routes/user.routes.js
+++ api/module/users/routes/user.routes.js
@@ -1,9 +1,9 @@
 import {Router} from 'express'
-
+import { UserController } from '../controllers/user.controllers' 
 const userRouter = Router()
 
-userRouter.get('/users/:userId')
-userRouter.patch('/users/:userId')
-userRouter.delete('/users/:userId')
+userRouter.get('/users/:userId', UserController.getUser)
+userRouter.patch('/users/:userId', UserController.updateUser)
+userRouter.delete('/users/:userId', UserController.deleteUser)
 
 export default userRouter;
