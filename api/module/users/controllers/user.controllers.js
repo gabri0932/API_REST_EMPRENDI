ba0791f891f0ca402fbf38a67a2b7f0739142f65
gabri0932 <gabrielgarces2432@gmail.com>
2025-02-17 00:03:31 +0100

    ...

--- api/module/users/controllers/user.controllers.js
+++ api/module/users/controllers/user.controllers.js
@@ -1,6 +1,6 @@
 export class UserController{
-    static getUser(){} //metodos son las cosas que puedes hacer en ese plano
-    static updateUser(){}
-    static deleteUser(){}
+    static getUser(req, res){} //metodos son las cosas que puedes hacer en ese plano
+    static updateUser(req, res){}
+    static deleteUser(req, res){}
 } //las clases son como planos en los cuales puedes hacer diferentes cosas
 
