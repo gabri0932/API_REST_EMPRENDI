715a1d5b2b13dfc84d346619e91ea140b4c82bd3
Darlin Arias <darlin.code@gmail.com>
2025-02-16 17:35:48 -0400

    conexion con la base de datos - correción 3

--- api/app/databases/mongo.db.js
+++ api/app/databases/mongo.db.js
@@ -6,8 +6,10 @@ const url = process.env.MONGO_URL // accedo a la variable de entorno del archivo
 export async function getMongoDB(){
     try {
         const client = new MongoClient(url) // creo un nuevo cliente
-        const database = client.db(process.env.DATABASE);
-        return database;
+        await client.connect(); // me conecto a la base de datos
+        const database = client.db(process.env.DATABASE); // selecciono la base de datos
+        
+        return database; // retorno la base de datos
     } catch (error) {
         console.error(error);
         throw error;
