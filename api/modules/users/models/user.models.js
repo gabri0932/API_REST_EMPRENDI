1e77c76f9f93c73910b85971548ad77624b108e7
gabri0932 <gabrielgarces2432@gmail.com>
2025-02-20 17:16:37 +0100

    create user

--- api/modules/users/models/user.models.js
+++ api/modules/users/models/user.models.js
@@ -1,8 +1,36 @@
 import { getMongoDB } from "../../../app/databases/mongo.db.js";
-
-class UserModels {
+const User_Collection = process.env.USERS_COLLECTION //obtengo la variable user collection del .env
+console.log(User_Collection)
+export class UserModels {
     static async GetUser(){}
-    static async CreateUser(){} 
+    static async GetUsers(){}
+    static async CreateUser({name, email, password}){
+        try{
+            const client = await getMongoDB() //obtengo el cliente de forma asincronica
+            const collection = client.collection(User_Collection)
+            const user = {
+                name,
+                email,
+                password,
+                createAt: Date.now(),
+                updatedAt: Date.now()
+            }
+            const result = collection.insertOne(user)
+            if(!result.acknowledged) return {success: false, error: {status:500}} //devuelve si salio mal
+            return {
+                success: true,
+                data: {
+                    _id: insertResult.insertId,
+                    ...user
+                } //si salio bien retorna nos datos, y el resultado sucess
+            }
+        }catch(error){
+            if(error instanceof Error){
+                Console.dir('Error in createUser():', error)
+            }
+            
+        }
+    } 
     static async UpdateUser(){}
     static async DeleteUser(){}
 }
