629a8802bd9fe3f79679ebcdd50b8fa3935f3e68
Darlin Arias <darlin.code@gmail.com>
2025-02-22 19:58:56 -0400

    userservice part 2 - correción.

--- api/modules/users/services/user.service.js
+++ api/modules/users/services/user.service.js
@@ -2,7 +2,7 @@ import { StringToObject } from "../../../shared/utils/stringToObjectId.js";
 import { UserModels } from "../models/user.models.js";
 
 export class UserService {
-    static async GetUser(_id){
+    static async GetUser({ _id }){
         const result = StringToObject(_id);
 
         if (!result.sucess) return {
@@ -33,18 +33,18 @@ export class UserService {
         }
     }
 
-    static async GetUsersByName(users) {
-        const findresult = await UserModels.GetUsersByName(users)
-        if(!findresult.success) return{
+    static async GetUsersByName({ name }) {
+        const findresult = await UserModels.GetUsersByName(name);
+
+        if (!findresult.success) return {
             success: false,
             error: {
-                status: findresult.error.status !== 500 ? 400:
-                findresult.error.status
+                status: findresult.error.status
             }
-            
-            
         }
-        const users = findresult.data
+
+        const { users } = findresult.data;
+
         return {
             success: true,
             data: {
@@ -52,4 +52,6 @@ export class UserService {
             }
         }
     }
+
+    static async GetUserByEmail({ email }) {}
 }
