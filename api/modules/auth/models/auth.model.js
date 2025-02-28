64731ddd344b329d7d8d89f92d81f13b921e5b6a
Darlin Arias <darlin.code@gmail.com>
2025-02-27 22:01:51 -0400

    auth model terminado - correción

--- api/modules/auth/models/auth.model.js
+++ api/modules/auth/models/auth.model.js
@@ -1,28 +1,27 @@
 import { getRedisClient } from "../../../app/databases/redis.db";
 
 export class AuthModel{
-    static async getSession(sessionId){
-        try{
-            const client = await getRedisClient()
-            const session = await client.get(sessionId)
-            if(!session){
-                return{
-                    sucess: falsa,
-                    error: {
-                        status: 404
-                    }
-                } 
-            }
-            return{
-                success: true,
-                data: {
-                    session: session
+    static async getSession({ sessionId }){
+        try {
+            const client = await getRedisClient();
+            const session = await client.get(sessionId);
+
+            if (!session) return {
+                success: falsa,
+                error: {
+                    status: 404
                 }
             }
-        }catch(error){
+
+            return {
+                success: true,
+                data: session
+            }
+        } catch (error) {
             if(error instanceof Error){
-                console.dir('Error in function getSession', error)
-                return{
+                console.dir('Error in function getSession:', error);
+
+                return {
                     success: false,
                     error:{
                         status: 500
@@ -31,33 +30,66 @@ export class AuthModel{
             }
         }
     }
-    static async createSesion(sessionId, token){
-        const client = await getRedisClient()
-        const session = await client.set(sessionId, token)
-        if(session !== 'OK'){
-            return{
-                success: false,
-                error:{
-                    status:500
+
+    static async createSession({ sessionId, token }){
+        try {
+            const client = await getRedisClient();
+            const session = await client.set(sessionId, token);
+
+            if (session !== 'OK') {
+                return{
+                    success: false,
+                    error: {
+                        status: 500
+                    }
                 }
             }
-        }return{
-            sucess: true,
-            data: null
-        } 
-    }
-    static async deleteSession(sessionId){
-        const client = await getRedisClient()
-        const deleteUserResult = client.del(sessionId)
-        if(!deleteUserResult) return {
-            success: false,
-            error: {
-                status: 500
+            
+            return {
+                success: true,
+                data: null
+            }
+        } catch (error) {
+            if(error instanceof Error){
+                console.dir('Error in function createSession:', error);
+
+                return {
+                    success: false,
+                    error:{
+                        status: 500
+                    }
+                }
             }
         }
-        return {
-            success: true,
-            data: null
+    }
+
+    static async deleteSession({ sessionId }){
+        try {
+            const client = await getRedisClient();
+            const deleteUserResult = client.del(sessionId);
+
+            if (!deleteUserResult) return {
+                success: false,
+                error: {
+                    status: 500
+                }
+            }
+
+            return {
+                success: true,
+                data: null
+            }
+        } catch (error) {
+            if(error instanceof Error){
+                console.dir('Error in function deleteSession:', error);
+
+                return {
+                    success: false,
+                    error:{
+                        status: 500
+                    }
+                }
+            }
         }
     }
 }
