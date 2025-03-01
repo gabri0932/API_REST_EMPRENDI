c6e39b2bebae3093d9ae315b2468fffedd8b6642
Darlin Arias <darlin.code@gmail.com>
2025-02-28 23:14:41 -0400

    method jwt - correción

--- api/shared/utils/jwtMethods.js
+++ api/shared/utils/jwtMethods.js
@@ -1,36 +1,45 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+const JWT_SECRET = process.env.JWT_SECRET
+
 export function singNewJet(TokenContent, expiresIn = "7"){
-   let token =""
-   try{
+    let token = "";
+
+    try {
         token = jwt.sign(
             TokenContent,
             JWT_SECRET,
             {expiresIn}
         )
-   }catch(error){
-    if(error instanceof Error){
-        console.error('Error in jwtMethod.singNewJet: ', error)
+    } catch(error) {
+        if(error instanceof Error){
+            console.error('Error in jwtMethod.singNewJet: ', error)
+        }
     }
-   }
-   return token
+
+    return token
 }
+
 export function verifyJwt(token){
-    const JWT_SECRET = process.env.JWT_SECRET
     let result = {
         success: false,
         code: UNAUTHORIZED
     }
-    try{
-        const payLoad = jwt
-        .verify(token, JWT_SECRET)
+
+    try {
+        const payLoad = jwt.verify(token, JWT_SECRET);
+
         result = {
             success:true,
             payLoad
         }
+
         return result
-    }catch(error){
+    } catch(error) {
         if(error instanceof Error){
             return result
         }
     }
+
     return result
 }
