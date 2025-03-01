80b5ac0cbab73f1fc0b916ffae587bf558beb976
gabri0932 <gabrielgarces2432@gmail.com>
2025-03-01 04:35:18 +0100

    service/authservice terminado

--- api/modules/auth/services/auth.service.js
+++ api/modules/auth/services/auth.service.js
@@ -1,5 +1,6 @@
 import { AuthModel } from "../models/auth.model";
 import { StringToObject } from '../../../shared/utils/stringToObjectId';
+import { singNewJet } from "../../../shared/utils/jwtMethods";
 
 export class authService{
     static async getSession({ sessionId }){
@@ -31,7 +32,20 @@ export class authService{
         }
     }
 
-    static async createSession(){}
+    static async createSession({userId}){
+        const sessionId = crypto.randomUUID()
+        const token = singNewJet(userId)
+        const createSessionResult = await AuthModel.createSession({sessionId, token})
+        if(!createSessionResult.success){
+            return{
+                success: false,
+                error: 'Its imposible create the session'
+            }
+        }return{
+            success: true,
+            data: sessionId
+        }
+    }
 
     static async deleteSession({ sessionId }) {
         const conversionResult = StringToObject(sessionId);
