7d066fa1dc12432ab0a3904f46e5d2d7a9d11c8f
gabri0932 <gabrielgarces2432@gmail.com>
2025-03-01 04:04:14 +0100

    method jwt

---
+++ api/shared/utils/jwtMethods.js
@@ -0,0 +1,36 @@
+export function singNewJet(TokenContent, expiresIn = "7"){
+   let token =""
+   try{
+        token = jwt.sign(
+            TokenContent,
+            JWT_SECRET,
+            {expiresIn}
+        )
+   }catch(error){
+    if(error instanceof Error){
+        console.error('Error in jwtMethod.singNewJet: ', error)
+    }
+   }
+   return token
+}
+export function verifyJwt(token){
+    const JWT_SECRET = process.env.JWT_SECRET
+    let result = {
+        success: false,
+        code: UNAUTHORIZED
+    }
+    try{
+        const payLoad = jwt
+        .verify(token, JWT_SECRET)
+        result = {
+            success:true,
+            payLoad
+        }
+        return result
+    }catch(error){
+        if(error instanceof Error){
+            return result
+        }
+    }
+    return result
+}
