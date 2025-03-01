8b30b40ed2f8038170f895cda8ff9f874828a2b7
Darlin Arias <darlin.code@gmail.com>
2025-03-01 11:50:03 -0400

    app/routes.js actualizado.

--- api/server.js
+++ api/server.js
@@ -3,16 +3,18 @@ import express from 'express';
 import apiRoutes from './app/routes';
 const app = express()
 
-
-app.use(apiRoutes)
-
+// Top level middlewares
 app.use(express.json());
 
+// Routes.
 app.get('/', (req, res) => {
     res.status(200).json({
         status: 200,
         message: 'API Working...'
     });
-})
+});
+app.use(apiRoutes);
+
+// Bottom level middlewares.
 
 export default app;
