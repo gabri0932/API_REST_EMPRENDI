4a596defd8b3bb3ce0966d528eff63e3e85f4533
gabri0932 <gabrielgarces2432@gmail.com>
2025-03-01 17:12:07 +0100

    auth routes terminado y auth controller en procesos

---
+++ api/modules/auth/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+import { authService } from "../services/auth.service";
+export class authController{
+    static async verify(req, res){
+    }
+    static async signin(req, res){}
+    static async signup(req, res){}
+    static async signout(req, res){}
+
+
+}
--- api/modules/auth/routes/auth.routes.js
+++ api/modules/auth/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { authController } from '../controllers/auth.controller';
 
 const authRouter = Router();
 
-authRouter.post('/verify-session', );
-authRouter.post('/signin', );
-authRouter.post('/signup', );
-authRouter.post('/signout', );
+authRouter.post('/verify-session', authController.verify );
+authRouter.post('/signin', authController.signin );
+authRouter.post('/signup', authController.signup );
+authRouter.post('/signout', authController.signout );
 
 export default authRouter;
