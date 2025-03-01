fe66e4c8ae25367579d46206bdbd751f1942923e
Darlin Arias <darlin.code@gmail.com>
2025-03-01 12:14:02 -0400

    app/routes.js actualizado 3.

--- api/modules/auth/controllers/auth.controller.js
+++ api/modules/auth/controllers/auth.controller.js
@@ -1,10 +1,8 @@
 import { authService } from "../services/auth.service";
-export class authController{
-    static async verify(req, res){
-    }
-    static async signin(req, res){}
-    static async signup(req, res){}
-    static async signout(req, res){}
 
-
+export class AuthController{
+    static async verify(req, res){};
+    static async signin(req, res){};
+    static async signup(req, res){};
+    static async signout(req, res){};
 }
--- api/modules/auth/routes/auth.routes.js
+++ api/modules/auth/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { authController } from '../controllers/auth.controller';
+import { AuthController } from '../controllers/auth.controller';
 
 const authRouter = Router();
 
-authRouter.post('/verify-session', authController.verify );
-authRouter.post('/signin', authController.signin );
-authRouter.post('/signup', authController.signup );
-authRouter.post('/signout', authController.signout );
+authRouter.post('/verify-session', AuthController.verify);
+authRouter.post('/signin', AuthController.signin);
+authRouter.post('/signup', AuthController.signup);
+authRouter.post('/signout', AuthController.signout);
 
 export default authRouter;
