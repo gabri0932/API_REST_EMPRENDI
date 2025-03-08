efee67e9ee6fbd12ad9932d14cc5ee088581d44a
Darlin Arias <darlin.code@gmail.com>
2025-03-07 22:13:09 -0400

    Changes done.

--- api/modules/auth/controllers/auth.controller.js
+++ api/modules/auth/controllers/auth.controller.js
@@ -4,20 +4,40 @@ import { validateUserInput } from "../validators/auth.validator";
 
 export class AuthController{
     static async verify(req, res){};
+
     static async signin(req, res){
-        const request = req.body
-        const validationResult = validateUserInput(request)
-        if(!validation.success){
-            res.status(400).json({status: 400, error: JSON.parse(validationResult.error.messange)})
+        const request = req.body;
+
+        const validationResult = validateUserInput(request);
+
+        if (!validation.success) {
+            res.status(400).json({
+                status: 400,
+                error: JSON.parse(validationResult.error.message)
+            });
+
+            return;
         }
-        const userToCreate = validationResult.data
-        const service = await UserService.GetUserByCredentials(userToCreate)
-        if(!service.success){
-            res.status(400).json({status:400, error:JSON.parse(service.error.messange)})
+
+        const userCredentials = validationResult.data;
+        
+        const findResult = await UserService.GetUserByCredentials(userCredentials);
+
+        if (!findResult.success) {
+            res.status(401).json({
+                status:401,
+                error: `User with provided credentials wasn't found.`
+            });
+
+            return;
         }
-        res.status(201).json({status:200, message: 'logged in sucessfully'})
-       
+
+        res.status(200).json({
+            status:200,
+            message: 'Logged in successfully.'
+        });
     };
+    
     static async signup(req, res){};
     static async signout(req, res){};
 }
--- api/modules/auth/validators/auth.validator.js
+++ api/modules/auth/validators/auth.validator.js
@@ -1,16 +1,19 @@
 import { z } from "zod";
 
-const userSchema = z.object({
-    name: z
-        .string({ required_error: "El nombre es obligatorio" })
-        .min(2, { message: "El nombre debe tener al menos 2 caracteres" })
-        .max(50, { message: "El nombre no puede superar los 50 caracteres" }),
+const userInputSchema = z.object({
     email: z
         .string({ required_error: "El email es obligatorio" })
         .email({ message: "El email no es válido" }),
     password: z
         .string({ required_error: "La contraseña es obligatoria" })
         .min(8, { message: "La contraseña debe tener al menos 8 caracteres" })
+});
+
+const userSchema = userInputSchema.extend({
+    name: z
+        .string({ required_error: "El nombre es obligatorio" })
+        .min(2, { message: "El nombre debe tener al menos 2 caracteres" })
+        .max(50, { message: "El nombre no puede superar los 50 caracteres" })
 });
 
 const userWithRoleSchema = userSchema.extend({
@@ -20,6 +23,10 @@ const userWithRoleSchema = userSchema.extend({
 })
 
 export function validateUserInput(content) {
+    return userInputSchema.safeParse(content);
+}
+
+export function validateUserCreation(content) {
     return userWithRoleSchema.safeParse(content);
 }
 
