e4446c7a21fbc3d012d9441bba9b4135a7830834
Darlin Arias <darlin.code@gmail.com>
2025-03-12 17:42:52 -0400

    Changes done.

--- api/modules/profiles/models/profiles.models.js
+++ api/modules/profiles/models/profiles.models.js
@@ -3,42 +3,46 @@ import { getMongoDB } from '../../../app/databases/mongo.db'
 const PROFILE_COLLECTION = process.env.PROFILE_COLLECTION
 
 export class ProfileModels{
-    static async getProfile({publicId}){
+    static async getProfile({ publicId }){
         try{
-            const client = await getMongoDB()
-            const collection = client.collection(PROFILE_COLLECTION)
-            const result = await collection.findOne({publicId})
-            if(!result){
-                return {
-                    success: false,
-                    error:{
-                        status: 400
-                    }
+            const client = await getMongoDB();
+            const collection = client.collection(PROFILE_COLLECTION);
+
+            const result = await collection.findOne({ publicId });
+
+            if (!result) return {
+                success: false,
+                error:{
+                    status: 404
                 }
             }
-            return{
+
+            return {
                 success: true,
                 data: result
             }
-
-        }catch(error){
-            if(error instanceof Error){
-                console.dir('Profile not found')
-                return{
+        } catch(error) {
+            if (error instanceof Error) {
+                console.log('Error in ProfileModels.getProfile:\n');
+                console.dir(error);
+                
+                return {
                     success: false,
                     error: {
                         status: 500
                     }
                 }
             }
-            return{
-                success: false,
-                error:{
-                    status: 500
-                }
+        }
+
+        return {
+            success: false,
+            error:{
+                status: 500
             }
         }
     }
+    
     static async getProfiles(){}
     static async updateProfile(){}
     static async deleteProfile(){}
