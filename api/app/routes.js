b3c6659bf1e89a5145271d362099c8c42adc8eb5
Darlin Arias <darlin.code@gmail.com>
2025-03-12 15:47:33 -0400

    Changes done.

--- api/app/routes.js
+++ api/app/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRoutes from '../modules/users/routes/user.routes.js';
 import authRoutes from '../modules/auth/routes/auth.routes.js';
+import profilesRoutes from '../modules/profiles/routes/profiles.routes.js';
 
 const apiRoutes = Router();
 
 apiRoutes.use('/auth', authRoutes);
 apiRoutes.use('/user', userRoutes);
+apiRoutes.use('/profiles', profilesRoutes);
 
 export default apiRoutes;
---
+++ api/modules/profiles/routes/profiles.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const profilesRouter = Router();
+
+profilesRouter.get('/', );
+profilesRouter.get('/:id', );
+profilesRouter.post('/', );
+profilesRouter.delete('/', );
+profilesRouter.put('/', );
+
+export default profilesRouter;
