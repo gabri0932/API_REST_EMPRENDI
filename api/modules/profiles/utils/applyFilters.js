41fe5299f90e804362be88c0ba538a80fb12281d
Darlin Arias <darlin.code@gmail.com>
2025-03-15 13:43:22 -0400

    Changes done.

---
+++ api/modules/profiles/utils/applyFilters.js
@@ -0,0 +1,18 @@
+const mongoQueries = {
+    name: (input) => ({ name: { $regex: input, $options: "i" } }),
+    service: (input) => ({ services: { $in: [ input ] } }),
+    technology: (input) => ({ technologies: { $in: [ input ] } }),
+    minPrice: (input) => ({ 'price.amount': { $gte: input } }),
+    maxPrice: (input) => ({ 'price.amount': { $lte: input } }),
+}
+
+export function applyFilters(filters) {
+    const queries = Object.entries(filters).reduce((acc, [key, value]) => {
+        if (key in mongoQueries) {
+            acc.push(mongoQueries[key](value))
+        }
+        return acc;
+    }, []);
+
+    return queries.length ? { $and: queries } : {};
+}
