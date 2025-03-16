c35e5a3f290a1755a5a79bbafc5c31b650007b17
Darlin Arias <darlin.code@gmail.com>
2025-03-16 11:14:05 -0400

    Next changes.

--- api/modules/profiles/controllers/profiles.controller.js
+++ api/modules/profiles/controllers/profiles.controller.js
@@ -1,5 +1,49 @@
+import { ProfilesService } from '../services/profiles.service';
+import { parseQueryParams } from '../../../shared/utils/parseQueryParams';
+
 export class ProfileController{
-    static async getProfiles(){}
+    static async getProfiles(req, res) {
+        if (!req.auth.user) {
+            res.status(400).json({
+                status: 400,
+                message: 'Unauthorized, you need being authenticated to process the request.'
+            });
+
+            return;
+        }
+
+        const { page, limit, ...filters } = parseQueryParams(req.query);
+
+        const skip = (page - 1) * limit;
+
+        const getProfilesResult = await ProfilesService.getProfiles({
+            skip,
+            limit,
+            filters
+        });
+
+        if (!getProfilesResult.success) {
+            res.status(500).json({
+                status: 500,
+                message: 'Internal Server Error.'
+            });
+
+            return;
+        }
+
+        const { profiles, count: profilesCount } = getProfilesResult.data;
+
+        res.status(200).json({
+            status: 200,
+            message: 'Profiles found successfully.',
+            page,
+            limit,
+            profilesCount,
+            data: {
+                profiles
+            }
+        });
+    }
     static async getProfileByPublicId(){}
     static async getProfilesSkills(){}
     static async getProfilesService(){}
