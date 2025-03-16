5da7c22902cccd2a1d0c2de4ea67ba97b48ce0fd
gabri0932 <gabrielgarces2432@gmail.com>
2025-03-16 16:01:53 +0100

    cositas

---
+++ api/modules/profiles/controllers/profiles.controller.js
@@ -0,0 +1,10 @@
+export class ProfileController{
+    static async getProfiles(){}
+    static async getProfileByPublicId(){}
+    static async getProfilesSkills(){}
+    static async getProfilesService(){}
+    static async createProfile(){}
+    static async hireProfile(){}
+    static async updateProfile(){}
+    static async deleteProfile(){}
+}
