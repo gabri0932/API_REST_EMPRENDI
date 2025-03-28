212b5ae29f74805f4dffbb48202427bfccc05fb6
Darlin Arias <darlin.code@gmail.com>
2025-03-28 14:44:01 -0400

    profiles - model: Obtener únicamente profiles freelancers.

--- api/modules/profiles/models/profiles.models.js
+++ api/modules/profiles/models/profiles.models.js
@@ -52,8 +52,8 @@ export class ProfilesModel {
 
             const appliedFilters = applyFilters(filters);
             const normalizedFilters = appliedFilters.length && appliedFilters.length > 1
-                ? { $and: appliedFilters }
-                : appliedFilters[0] || {};
+                ? { $and: { ...appliedFilters, role: 'freelancer' }
+                : { ...appliedFilters[0], role: 'freelancer' || { role: 'freelancer' };
 
             const getProfilesResult = await collection.find(
                 normalizedFilters
