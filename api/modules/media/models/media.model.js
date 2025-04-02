a58057de2e6800947a811ec03b0a9a80bfc402e7
Darlin Arias <darlin.code@gmail.com>
2025-04-01 20:25:25 -0400

    Media module - (4/?).

--- api/modules/media/models/media.model.js
+++ api/modules/media/models/media.model.js
@@ -5,27 +5,40 @@ import {
 } from '../../../app/databases/supabase.db.js';
 
 export class MediaModel {
-    static async saveAvatarImage({ fileName, fileType, file }) {
+    static async saveImage({ path, file, fileType }) {
+        const client = await getStorageClient();
+        const path = `${SUPABASE_AVATARS_FOLDER}/${fileName}`;
+
+        const { data, error } = await client.upload(
+            path,
+            file,
+            { contentType: fileType }
+        );
+
+        if (error || !data) return {
+            success: false,
+            error: {
+                status: 500
+            }
+        }
+
+        return {
+            success: true,
+            data
+        }
+    }
+
+    static async saveAvatarImage({ fileName, file, fileType }) {
         try {
-            const client = await getStorageClient();
+            const path = `${SUPABASE_AVATARS_FOLDER}/${fileName}`;
 
-            const { data, error } = await client.upload(
-                `${SUPABASE_AVATARS_FOLDER}/${fileName}`,
+            const result = await this.saveImage({
+                path,
                 file,
-                { contentType: fileType }
-            );
+                fileType
+            });
 
-            if (error || !data) return {
-                success: false,
-                error: {
-                    status: 500
-                }
-            }
-
-            return {
-                success: true,
-                data
-            }
+            return result;
         } catch (error) {
             if (error instanceof Error) {
                 console.log('Error in MediaModel.saveAvatarImage():\n');
@@ -41,15 +54,37 @@ export class MediaModel {
         }
     }
 
-    static async saveCoverImage({ fileName, fileType, file }) {
+    static async saveCoverImage({ fileName, file, fileType }) {
+        try {
+            const path = `${SUPABASE_COVERS_FOLDER}/${fileName}`;
+
+            const result = await this.saveImage({
+                path,
+                file,
+                fileType
+            });
+
+            return result;
+        } catch (error) {
+            if (error instanceof Error) {
+                console.log('Error in MediaModel.saveCoverImage():\n');
+                console.dir(error, { depth: null });
+
+                return {
+                    success: false,
+                    error: {
+                        status: 500
+                    }
+                }
+            }
+        }
+    }
+
+    static async getImage({ path }) {
         try {
             const client = await getStorageClient();
 
-            const { data, error } = await client.upload(
-                `${SUPABASE_COVERS_FOLDER}/${fileName}`,
-                file,
-                { contentType: fileType }
-            );
+            const { error, data } = await client.download(path);
 
             if (error || !data) return {
                 success: false,
@@ -64,7 +99,39 @@ export class MediaModel {
             }
         } catch (error) {
             if (error instanceof Error) {
-                console.log('Error in MediaModel.saveCoverImage():\n');
+                console.log('Error in MediaModel.getImage():\n');
+                console.dir(error, { depth: null });
+
+                return {
+                    success: false,
+                    error: {
+                        status: 500
+                    }
+                }
+            }
+        }
+    }
+
+    static async deleteImage({ path }) {
+        try {
+            const client = await getStorageClient();
+
+            const { error } = await client.remove([ path ]);
+
+            if (error) return {
+                success: false,
+                error: {
+                    status: 500
+                }
+            }
+
+            return {
+                success: true,
+                data: null
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                console.log('Error in MediaModel.deleteImage():\n');
                 console.dir(error, { depth: null });
 
                 return {
